50bbe60fe8eebc61bf0ee88d967713f1511822d2
Youmayu <goldenmaker2000@gmail.com>
2023-09-21 13:29:21 +0200

    Startup

--- Neopixel_game/Neopixel_game.ino
+++ Neopixel_game/Neopixel_game.ino
@@ -2,3 +2,41 @@
 #define PIN 3
 #define NUMPIXELS 32
 
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void startup(){
+    pixels.setPixelColor(15,pixels.Color(255,0,0));
+    pixels.setPixelColor(16,pixels.Color(0,0,255));
+    pixels.show();
+    delay(1000);
+    for (int i=0; i<3; i++){
+        pixels.clear();
+        pixels.show();
+        delay(100);
+        pixels.setPixelColor(15,pixels.Color(255,0,0));
+        pixels.setPixelColor(16,pixels.Color(0,0,255));
+        pixels.show();
+        delay(300);
+    }
+    for (int i=0; i<NUMPIXELS/2; i++){
+        pixels.setPixelColor(15-i,pixels.Color(255,0,0));
+        pixels.setPixelColor(16+i,pixels.Color(0,0,255));
+        pixels.show();
+        delay(50);
+        pixels.clear();
+        pixels.show();
+    }
+    pixels.setPixelColor(0,pixels.Color(255,0,0));
+    pixels.setPixelColor(31,pixels.Color(0,0,255));
+    pixels.show();
+}
+
+void setup() {
+    pixels.begin();
+    startup();
+}
+
+
+
+void loop(){
+}
