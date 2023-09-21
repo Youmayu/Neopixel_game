c90f71c5801fe9460c4cb9782695bae3fda67785
Youmayu <goldenmaker2000@gmail.com>
2023-09-21 14:31:15 +0200

    Chooses who serves

--- Neopixel_game/Neopixel_game.ino
+++ Neopixel_game/Neopixel_game.ino
@@ -4,6 +4,12 @@
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 unsigned long int oldmillis=0;
+unsigned long int interval=0;
+int start=0;
+
+int PongStartInterval=500;
+bool PongStart=false;
+int x=0;
 
 void startup(){
     pixels.setPixelColor(15,pixels.Color(255,0,0));
@@ -32,14 +38,46 @@ void startup(){
     pixels.show();
 }
 
-void pong(){
-    oldmillis=millis();
-    
+void pongrandom(){
+    interval=PongStartInterval;
+    start=random(0,2);
+    Serial.println(start);
+    if (start==0){
+        while (x<=11){
+            pixels.setPixelColor(15, pixels.Color(255,255,255));
+            pixels.setPixelColor(16, pixels.Color(0,0,0));
+            pixels.show();
+            delay(PongStartInterval);
+            pixels.setPixelColor(15, pixels.Color(0,0,0));
+            pixels.setPixelColor(16, pixels.Color(255,255,255));
+            pixels.show();
+            delay(PongStartInterval);
+            x+=1;
+            PongStartInterval-=50;
+        }
+
+    }
+    if (start==1){
+        while (x<=11){
+            pixels.setPixelColor(15, pixels.Color(0,0,0));
+            pixels.setPixelColor(16, pixels.Color(255,255,255));
+            pixels.show();
+            delay(PongStartInterval);
+            pixels.setPixelColor(15, pixels.Color(255,255,255));
+            pixels.setPixelColor(16, pixels.Color(0,0,0));
+            pixels.show();
+            delay(PongStartInterval);
+            x+=1;
+            PongStartInterval-=50;
+        }
+    }
 }
 
 void setup() {
     pixels.begin();
+    Serial.begin(9600);
     startup();
+    pongrandom();
 }
 
 void loop(){
